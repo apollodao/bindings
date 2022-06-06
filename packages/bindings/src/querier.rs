eacdcdcd711d04de5093f5baef4f27d767aa5ef7
George McAskill <42020095+georgemc98@users.noreply.github.com>
2022-06-06 17:45:57 -0500

    feat: add query pool state wrapper

--- packages/bindings/src/querier.rs
+++ packages/bindings/src/querier.rs
@@ -1,15 +1,27 @@
-use cosmwasm_std::{QuerierWrapper, QueryRequest, StdResult};
+use cosmwasm_std::{
+    from_binary, to_binary, Querier, QuerierResult, QuerierWrapper, QueryRequest, StdResult,
+};
 
-use crate::query::{FullDenomResponse, OsmosisQuery};
+use crate::query::{FullDenomResponse, OsmosisQuery, PoolStateResponse};
 
 /// This is a helper wrapper to easily use our custom queries
 pub struct OsmosisQuerier<'a> {
     querier: &'a QuerierWrapper<'a, OsmosisQuery>,
 }
 
+impl Querier for OsmosisQuerier<'_> {
+    fn raw_query(&self, bin_request: &[u8]) -> QuerierResult {
+        self.querier.raw_query(bin_request)
+    }
+}
+
 impl<'a> OsmosisQuerier<'a> {
     pub fn new(querier: &'a QuerierWrapper<OsmosisQuery>) -> Self {
         OsmosisQuerier { querier }
+    }
+
+    pub fn query_pool_state(&self, pool_id: u64) -> StdResult<PoolStateResponse> {
+        QuerierWrapper::new(self).query(&OsmosisQuery::PoolState { id: pool_id }.into())
     }
 
     pub fn full_denom(
